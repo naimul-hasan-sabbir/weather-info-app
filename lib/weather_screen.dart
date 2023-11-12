11cb2f9e628d76b5edbbe2df3e704a5c7c7c49d7
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-11-12 23:23:37 +0600

    updated

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -78,15 +78,20 @@ class WeatherScreen extends StatelessWidget {
               SizedBox(
                 width: 100,
                 child: Card(
-                  child: Column(
-                    children: [
-                      Text("10:40", style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),),
-                      const SizedBox(height: 8,),
-                      Icon(Icons.cloud, size: 32,),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("10:40", style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        const SizedBox(height: 8,),
+                        Icon(Icons.cloud, size: 32,),
+                        const SizedBox(height: 8,),
+                        Text("320.12"),
+                      ],
+                    ),
                   ),
                 ),
               )

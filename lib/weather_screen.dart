2d6818da7b85080dd456e3ab1ce483dc467087d9
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-11-16 23:41:12 +0600

    updated

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -280,3 +280,42 @@ class WeatherScreen extends StatelessWidget {
     );
   }
 }
+
+class HourlyForecastWidge extends StatelessWidget {
+  const HourlyForecastWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                    child: const Column(
+                      children: [
+                        Text(
+                          "10:40",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text("320.12"),
+                      ],
+                    ),
+                  ),
+    );
+  }
+}

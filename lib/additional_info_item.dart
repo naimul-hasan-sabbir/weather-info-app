52a196df86d2ffa679c11ea0d9fe32c210268c48
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-12-01 21:57:47 +0600

    updated

--- lib/additional_info_item.dart
+++ lib/additional_info_item.dart
@@ -2,28 +2,33 @@ import "package:flutter/material.dart";
 
 class AdditionalInfoItem extends StatelessWidget {
   final IconData icon;
+  final String label;
+  final String value;
   const AdditionalInfoItem({
     super.key,
+    required this.icon,
+    required this.label,
+    required this.value,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
+      children: [
         Icon(
           icon,
           size: 32,
         ),
-        SizedBox(
+        const SizedBox(
           height: 8,
         ),
-        Text("Humidity"),
-        SizedBox(
+        Text(label),
+        const SizedBox(
           height: 8,
         ),
         Text(
-          "91",
-          style: TextStyle(
+          value,
+          style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
           ),

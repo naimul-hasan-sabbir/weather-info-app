3814ad9e255b30bc724e4e82f68bd5d1452b1e05
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-12-17 23:14:47 +0600

    updated

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -10,7 +10,10 @@ class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   Future getCurrentWeather() async {
-    http.get(Uri.parse(uri))
+    http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=72697bf4fcb7d4d98def70718d11c62f"),
+    );
   }
 
   @override
